34051db072f00130c950fb58b30ae4b9ea956fa4
uma0312 <uma0312@server.fake>
2017-08-09 13:50:39 +0900

    Update

--- #2/vending_machine.rb
+++ #2/vending_machine.rb
@@ -1,10 +1,12 @@
 class VendingMachine
   attr_accessor :items, :permitted_money, :inserted_money
+  attr_reader :sales
 
   def initialize()
     @items = Hash.new{|h, k| h[k] = []}
     @permitted_money = [10, 50, 100, 500, 1000]
     @inserted_money = 0
+    @sales = 0
 
     5.times{self.add(Drink.new("コーラ", 120))}
   end
@@ -22,9 +24,11 @@ class VendingMachine
   end
 
   def purchase(name)
-    if @inserted_money >= @items[name][0].price
-      @inserted_money -= @items[name][0].price
+    price = @items[name][0].price
+    if @inserted_money >= price
+      @inserted_money -= price
       @items[name].delete_at(0) # Delete a purchased item
+      @sales += price
       self.return_change
     else
       puts "#{name} is not purchasable!"
